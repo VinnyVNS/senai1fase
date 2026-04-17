adcb4a1d3ce2e47cf541a52ce25452db740ca889
VinnyVNS <vinisouzavns@gmail.com>
2026-04-16 22:10:04 -0300

    testes com java

---
+++ 01-javascript/testes.js
@@ -0,0 +1,20 @@
+// alert("Olá mundo!")
+
+// let nome, idade
+
+// nome = prompt("Digite seu nome: ")
+// idade = prompt("Digite sua idade: ")
+
+// alert("Nome: "+nome+"\nIdade: "+idade)
+
+let nota1, nota2, soma
+
+nota1 = prompt("Digite a nota 1: ")
+nota1 = Number(nota1)
+
+nota2 = prompt("Digete a nota 2: ")
+nota2 = Number(nota2)
+
+soma = nota1 + nota2
+
+alert("Sua nota é: "+soma)
