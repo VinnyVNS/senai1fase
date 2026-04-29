fbf0720b9263a0b1ae44580f08690de051986e5a
VinnyVNS <vinisouzavns@gmail.com>
2026-04-29 17:00:59 -0300

    update

--- 01-javascript/senai/01-senai/romerobrique.js
+++ 01-javascript/senai/01-senai/romerobrique.js
@@ -1,9 +1,14 @@
 function calcularBrique(){
-    let arte, preco
+    document.getElementById("resultado").innerHTML = ``
+    document.getElementById("resultado2").innerHTML = ``
+    
+    let arte, preco, lucro
 
     arte = Number(prompt("Digite quanto você pagou na arte:"))
 
+    lucro = arte * 2
     preco = arte * 3
 
-    document.getElementById("resultado").innerHTML = "O valor final será de R$"+preco.toFixed(2)
+    document.getElementById("resultado").innerHTML = `O valor final da arte será de R$${preco}.`
+    document.getElementById("resultado2").innerHTML = `<br>O lucro será de R$${lucro}.`
 }
