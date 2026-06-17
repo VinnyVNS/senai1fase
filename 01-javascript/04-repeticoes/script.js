adb0e435a4bfe5cf766346aa687fd66a7b4ea5f2
VinnyVNS <vinisouzavns@gmail.com>
2026-06-16 22:11:47 -0300

    update

---
+++ 01-javascript/04-repeticoes/script.js
@@ -0,0 +1,59 @@
+// let y = 1
+
+// for(i = 1; y <= 10; i++){
+
+//     let valor = i * y
+//     document.getElementById("texto").innerHTML += `${valor} <br>`
+//     y++
+
+// }
+
+// let y = 10
+
+// for(i = 10; y >= 1; i--){
+
+//     document.getElementById("texto").innerHTML += `${i} <br>`
+//     y--
+// }
+
+// let y = 1
+
+// let total = 0
+
+// for(i = 1; y <= 100; i++){
+
+//     total = total + i
+//     y++
+// }
+
+// document.getElementById("texto").innerHTML = `${total}`
+
+
+// for(i = 1; i <= 20; i++){
+
+//     let valor = i % 2
+
+//     if(valor == 1){
+//         document.getElementById("texto").innerHTML += `${i} <br>`
+//     }
+// }
+
+
+// for(i = 1; i <= 100; i++){
+
+//     let multi3 = i % 3
+//     let multi5 = i % 5
+
+//     if(multi3 == 0 && multi5 == 0){
+//         document.getElementById("texto").innerHTML += `${i} FizzBuzz <br>`
+//     }
+//     else if(multi3 == 0){
+//         document.getElementById("texto").innerHTML += `${i} Fizz <br>`
+//     }
+//     else if(multi5 == 0){
+//         document.getElementById("texto").innerHTML += `${i} Buzz <br>`
+//     }
+//     else{
+//         document.getElementById("texto").innerHTML += `${i} <br>`
+//     }
+// }
