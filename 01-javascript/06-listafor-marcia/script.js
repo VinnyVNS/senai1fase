33b2435076a8ad5df8c0cb2201fad659d5d31cf2
VinnyVNS <vinisouzavns@gmail.com>
2026-06-25 19:41:01 -0300

    update

--- 01-javascript/06-listafor-marcia/script.js
+++ 01-javascript/06-listafor-marcia/script.js
@@ -1,100 +1,11 @@
-// let i = 1
-
-// while(i <= 10){
-
-//     let valor = i * i
-//     document.getElementById("texto").innerHTML += `${valor} <br>`
-//     i++
-// }
-
-
-// for(i = 10; i >= 1; i--){
-
-//     document.getElementById("texto").innerHTML += `${i} <br>`
-// }
-
-
-// let i = 1
-
-// let total = 0
-
-// while(i <= 100){
-
-//     total = total + i
-//     i++
-// }
-
-// document.getElementById("texto").innerHTML = `${total}`
-
-
-// for(i = 1; i <= 20; i++){
-
-//     let valor = i % 2
-
-//     if(valor == 1){
-//         document.getElementById("texto").innerHTML += `${i} <br>`
-//     }
-// }
-
-
-// let i = 1
-
-// while(i <= 100){
-
-//     let multi3 = i % 3
-//     let multi5 = i % 5
-
-//     if(multi3 == 0 && multi5 == 0){
-//         document.getElementById("texto").innerHTML += `${i} FizzBuzz <br>`
-//     }
-//     else if(multi3 == 0){
-//         document.getElementById("texto").innerHTML += `${i} Fizz <br>`
-//     }
-//     else if(multi5 == 0){
-//         document.getElementById("texto").innerHTML += `${i} Buzz <br>`
-//     }
-//     else{
-//         document.getElementById("texto").innerHTML += `${i} <br>`
-//     }
-
-//     i++
-// }
-
-// let numero = Number(prompt("Digite um numero"))
-
-// let fator = 1
-
-// for(i = 1; i <= numero; i++){
-
-//     fator = fator * i
-// }
-
-// document.getElementById("texto").innerHTML += `${fator} <br>`
-
-// for(i = 2; i <= 50; i++){
-//     let primo = true
-
-//     for(y = 2; y < i; y++){
-//         let valor = i % y
-
-//         if(valor === 0){
-//             primo = false
-//         }
-//     }
-
-//     if(primo === true){
-//         document.getElementById("texto").innerHTML += `${i} <br>`
-//     }
-// }
-
 // ==============================================================================================
 
-// let soma = 0
+let soma = 0
 
-// for(i = 1; i <= 10; i++){
-//     soma = soma + i;
-//     document.getElementById("texto").innerHTML += `${soma} <br>`
-// }
+for(i = 1; i <= 10; i++){
+    soma = soma + i;
+    document.getElementById("texto").innerHTML += `${soma} <br>`
+}
 
 // ============================================================================================
 
