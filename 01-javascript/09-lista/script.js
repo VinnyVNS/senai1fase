ca32bf76493081b15250c935125a948b85accc9d
VinnyVNS <vinisouzavns@gmail.com>
2026-07-07 22:13:04 -0300

    update

--- 01-javascript/09-lista/script.js
+++ 01-javascript/09-lista/script.js
@@ -1,21 +1,46 @@
-let nomes = []
-// let contagemFila = 0
+const nomes = []
+
+const form = document.getElementById("form")
 
 function addNome(){
-    let nomeNovo = prompt("Digite seu nome:")
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+    })
 
-    nomes.push(nomeNovo)
-
-    // document.getElementById("lista").innerHTML += `${nomeNovo}: ${contagemFila}<br>`
-    // contagemFila++
-}
-
-function verificarPos(){
-    let nomeUser = prompt("Digite seu nome:")
+    const nomeNovo = document.getElementById("inputNome").value
 
     for(let i = 0; i < nomes.length; i++){
-        if(nomeUser == nomes[i]){
-            document.getElementById("resultado").innerHTML = `Você está na posição: ${i}`
+        if(nomes[i].toLowerCase() == nomeNovo.toLowerCase()){
+            alert("O nome já foi registrado.")
+            return
         }
     }
+
+    if(/\d/.test(nomeNovo)){
+        alert("Digite um nome sem números.")
+        return
+    }
+    else{
+        nomes.push(nomeNovo)
+    }
+    
+
+    for(let i = 0; i < nomes.length; i++){
+        if(nomes[i].toLowerCase() == nomeNovo.toLowerCase()){
+            document.getElementById("seletor").innerHTML += `<option value="${i}">${nomes[i]} [${i}]</option>`
+        }
+        
+    }
+
+    document.getElementById("inputNome").value = ``
 }
+
+// function verificarPos(){
+//     const nomeUser = document.getElementById("nome").value
+
+//     for(let i = 0; i < nomes.length; i++){
+//         if(nomeUser == nomes[i]){
+//             document.getElementById("resultado").innerHTML = `Você está na posição: ${i}`
+//         }
+//     }
+// }
