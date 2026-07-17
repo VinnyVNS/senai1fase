6af4a82e6ecf1418e3ead41a1fbd22f3e304260e
VinnyVNS <vinisouzavns@gmail.com>
2026-07-16 22:00:42 -0300

    update

---
+++ 01-javascript/11-ListaBateria/script.js
@@ -0,0 +1,651 @@
+// const personagens = ["Lúcio Fernando", "Mônica", "Capitão Ganso"];
+
+// personagens.push("Gill Bates");
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = ["Lúcio Fernando", "Mônica", "Capitão Ganso"];
+
+// personagens.unshift("Dona Bete");
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates"
+// ];
+
+// personagens.pop();
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates"
+// ];
+
+// personagens.splice(0, 1);
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates",
+//     "Junin"
+// ];
+
+// personagens.splice(2, 1);
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates",
+//     "Junin"
+// ];
+
+// personagens.splice(3, 2);
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney"
+// ];
+
+// personagens.push("Padre Ernan Buco");
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney"
+// ];
+
+// personagens.unshift("GENéZio", "Kowalski");
+
+// console.log(personagens);
+
+// ================================================================================================
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney",
+//     "GENéZio"
+// ];
+
+// console.log(personagens.indexOf("Waldisney"));
+
+// ================================================================================================
+
+// const personagens = [
+//     "Peba",
+//     "Bilu",
+//     "Waldisney",
+//     "GENéZio"
+// ];
+
+// console.log(personagens.includes("Heitor Tuga"));
+
+// ================================================================================================
+
+// const personagens = [
+//     "Lúcio Fernando",
+//     "Mônica",
+//     "Capitão Ganso",
+//     "Gill Bates"
+// ];
+
+// personagens.reverse();
+
+// console.log(personagens);
+
+// ================================================================================================
+
+const personagens = [
+    "Tião",
+    "Junin",
+    "Padre Ernan Buco"
+];
+
+// const pessoas = ["Dona Bete", "Junin", "Gael", "Mônika", "Gill Bates"];
+
+// pessoas.push("Capitão Ganso");
+
+// console.log(pessoas);
+
+// console.log(pessoas.includes("Mônika"));
+
+// ================================================================================================
+
+// const empresas = [
+//   "Macrohard",
+//   "Hotel Téo",
+//   "Pizza Ruth",
+//   "HD Cimentos",
+//   "Navegano",
+//   "Roller Store",
+//   "Be a Bar",
+//   "Indra",
+//   "Acarajéssica",
+//   "Reciclaudio"
+// ];
+
+// empresas.splice(0, 1);
+
+// console.log(empresas);
+
+// console.log(empresas.indexOf("HD Cimentos"));
+
+// ================================================================================================
+
+// const pessoas = ["Kowalski", "Heitor Tuga", "Bilu", "Rita Aline", "Caio Pontes", "PeBa"];
+
+// pessoas.unshift("Frei Dimão");
+
+// pessoas.pop();
+
+// console.log(pessoas);
+
+// ================================================================================================
+
+// const empresas = [
+//   "Telles Transportes",
+//   "Rede Eco",
+//   "Pousada Ada",
+//   "Resort Orth",
+//   "Bergamota",
+//   "Amazonia",
+//   "Coxão do Santinho",
+//   "Casa de Pastel",
+//   "Rango Star",
+//   "Alibebe",
+//   "Ed Som",
+//   "Gê Estantes"
+// ];
+
+// empresas.splice(5 , 1);
+
+// console.log(empresas);
+
+// console.log(empresas.includes("Amazonia"));
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Márcia",
+//   "Mano Juca",
+//   "Lúcio Fernando",
+//   "Roque",
+//   "Yasmina",
+//   "Waldisney",
+//   "Lady Murphy"
+// ];
+
+// pessoas.reverse();
+
+// console.log(pessoas);
+
+// console.log(pessoas.indexOf("Roque"));
+
+// ================================================================================================
+
+// const empresas = [
+//   "Harry Plotter",
+//   "Albergue Berg",
+//   "Camping Pim",
+//   "Ron Bernardo Pet Shop",
+//   "Visionery",
+//   "Armando Guerra",
+//   "Plumas de Avalon",
+//   "Romero Brique",
+//   "Meia Meia Meia",
+//   "Taberna Periódica",
+//   "Tião Megastore",
+//   "Milhion Pamonharia",
+//   "Templo de Salmão"
+// ];
+
+// empresas.push("Banda Jota Qwert");
+
+// empresas.unshift("Rock in Reels");
+
+// empresas.reverse();
+
+// console.log(empresas);
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Guilherme Portões",
+//   "Jeferson Bezerra",
+//   "Pe. Ernan Buco",
+//   "Ron Roni",
+//   "Seu Bernardo",
+//   "Luiza",
+//   "Van Grogue",
+//   "Rosa Negra"
+// ];
+
+// let pessoaRandom1 = Number(Math.floor(Math.random() * pessoas.length));
+// let pessoaRandom2 = Number(Math.floor(Math.random() * pessoas.length));
+
+// console.log(pessoas);
+
+// pessoas.splice(pessoaRandom1, 1);
+// pessoas.splice(pessoaRandom2, 1);
+
+// pessoas.push("Helen Sater");
+
+// console.log(pessoas);
+
+// ================================================================================================
+
+// const empresas = [
+//   "Grades Cida",
+//   "Food Park Comidas e Clichês",
+//   "Churrascaria do Gaúcho",
+//   "Batata da Tabata Batataria",
+//   "Lanchonete Altas Horas",
+//   "Tio do Pavê Sobremesas",
+//   "Bolores Bolos e Flores",
+//   "Sanduíche-iche da Ruth",
+//   "Cereal Killer Cereal Bar",
+//   "Restaurante Contra-Filé"
+// ];
+
+// console.log(empresas.indexOf("Lanchonete Altas Horas"));
+
+// empresas.splice(4, 1);
+
+// console.log(empresas);
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Alan Grande",
+//   "Tio Paulo",
+//   "Dr Auzio",
+//   "GENéZio",
+//   "Mosta",
+//   "Guarda Napoleão"
+// ];
+
+// console.log(pessoas.includes("GENéZio"));
+
+// pessoas.splice(0, 1);
+
+// pessoas.reverse();
+
+// console.log(pessoas);
+
+// ================================================================================================
+
+// const empresas = [
+//   "Pamdiló Bolos",
+//   "Moçambike",
+//   "Decorações de Coração",
+//   "Clínica Frida Calos",
+//   "Taubaté Tábuas",
+//   "Vidraçaria Douglass",
+//   "Gil Som",
+//   "Paty em Ação",
+//   "Mi Sungas",
+//   "Sombransheila",
+//   "Esthertica",
+//   "Pricílios",
+//   "Hanna Barbearia",
+//   "Realisa Alisamentos"
+// ];
+
+// empresas.unshift("Floricultura Jorge Tadeu");
+
+// empresas.splice(7, 1);
+
+// console.log(empresas);
+
+// ================================================================================================
+
+// const pessoas = ["Dona Bete", "Kowalski", "Capitão Ganso", "Junin", "Bilu"];
+
+// pessoas.push("Gill Bates");
+
+// console.log(pessoas);
+
+// console.log(pessoas.includes("Kowalski"));
+
+// console.log(pessoas.indexOf("Kowalski"));
+
+// ================================================================================================
+
+// const empresas = [
+//   "Roda de Roque",
+//   "MilkSahakespeare",
+//   "Dasamassandro",
+//   "Tias Fofinhas",
+//   "Horrível Trio",
+//   "Pedro Chips",
+//   "Frutos e Fritas do Frota",
+//   "Taxi Aéreo Mamute Pequenino",
+//   "Adomicílios",
+//   "Abalada",
+//   "Ricárdio"
+// ];
+
+// empresas.pop();
+
+// empresas.unshift("Cardiopatinha");
+
+// console.log(empresas);
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Heitor Tuga",
+//   "Frei Dimão",
+//   "Gael",
+//   "Sarumano",
+//   "Márcia",
+//   "Mônika",
+//   "Yasmina",
+//   "Lady Murphy",
+//   "Caio Pontes"
+// ];
+
+// pessoas.reverse();
+
+// console.log(pessoas);
+
+// console.log(pessoas.includes("Sarumano"));
+
+// ================================================================================================
+
+// const empresas = [
+//   "A Bem Suada",
+//   "Aspirantes Aspiradores",
+//   "jamanjo.com.br",
+//   "MA Canudos",
+//   "IA",
+//   "Pé Pequeno",
+//   "Trajeto Pomar",
+//   "Vírus da Grife",
+//   "Paralamas do Seu Celso",
+//   "Samara Poços"
+// ];
+
+// empresas.splice(8, 2);
+
+// empresas.push("Loja de sucos Uso Fruto");
+
+// console.log(empresas);
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Lúcio Fernando",
+//   "Ron Roni",
+//   "Seu Bernardo",
+//   "Luiza",
+//   "Roque",
+//   "Rita Aline",
+//   "Dr Auzio"
+// ];
+
+// pessoas.splice(0, 1);
+// pessoas.pop();
+
+// console.log(pessoas)
+
+// console.log(pessoas.indexOf("Luiza"));
+
+// ================================================================================================
+
+// const empresas = [
+//   "Hotel Téo",
+//   "Pousada Ada",
+//   "Resort Orth",
+//   "Albergue Berg",
+//   "Camping Pim",
+//   "Macrohard",
+//   "Bergamota",
+//   "Amazonia",
+//   "Visionery",
+//   "HD Cimentos",
+//   "Meia Meia Meia",
+//   "Coxão do Santinho"
+// ];
+
+// empresas.unshift("Rede Eco");
+
+// console.log(empresas);
+
+// console.log(empresas.includes("Macrohard"));
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Pe. Ernan Buco",
+//   "Frei Dimão",
+//   "Capitão Ganso",
+//   "Junin",
+//   "PeBa",
+//   "Mano Juca",
+//   "Bilu",
+//   "Waldisney"
+// ];
+
+// pessoas.splice(4, 1);
+// pessoas.reverse();
+
+// console.log(pessoas);
+
+// ================================================================================================
+
+// const empresas = [
+//   "Casa de Pastel",
+//   "Lanchonete Altas Horas",
+//   "Tio do Pavê Sobremesas",
+//   "Bolores Bolos e Flores",
+//   "Pizza Ruth",
+//   "Sanduíche-iche da Ruth",
+//   "Cereal Killer Cereal Bar",
+//   "Restaurante Contra-Filé",
+//   "Rango Star",
+//   "Acarajéssica",
+//   "Templo de Salmão",
+//   "Navegano",
+//   "Alibebe"
+// ];
+
+// empresas.push("Pamdiló Bolos");
+
+// console.log(empresas);
+// console.log(empresas.indexOf("Pizza Ruth"));
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Guilherme Portões",
+//   "Jeferson Bezerra",
+//   "Heitor Tuga",
+//   "Gael",
+//   "Sarumano",
+//   "Márcia"
+// ];
+
+// pessoas.splice(2, 2);
+
+// console.log(pessoas);
+
+// console.log(pessoas.length);
+
+// ================================================================================================
+
+// const empresas = [
+//   "Clínica Frida Calos",
+//   "Roller Store",
+//   "Taubaté Tábuas",
+//   "Vidraçaria Douglass",
+//   "Ed Som",
+//   "Gil Som",
+//   "Paty em Ação",
+//   "Mi Sungas",
+//   "Sombransheila",
+//   "Esthertica",
+//   "Pricílios",
+//   "Hanna Barbearia",
+//   "Realisa Alisamentos"
+// ];
+
+// empresas.unshift("Decorações de Coração");
+// empresas.pop();
+// empresas.reverse();
+
+// console.log(empresas);
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Alan Grande",
+//   "Helen Sater",
+//   "Tio Paulo",
+//   "Waldisney",
+//   "Dr Auzio",
+//   "GENéZio",
+//   "Gill Bates",
+//   "Lady Murphy",
+//   "Caio Pontes",
+//   "Mosta"
+// ];
+
+// console.log(pessoas);
+
+// console.log(pessoas.includes("Gill Bates"));
+
+// pessoas.splice(6, 1);
+
+// console.log(pessoas);
+
+// ================================================================================================
+
+// const empresas = [
+//   "Roda de Roque",
+//   "MilkSahakespeare",
+//   "Dasamassandro",
+//   "Banda Jota Qwert",
+//   "Tias Fofinhas",
+//   "Horrível Trio",
+//   "Pedro Chips",
+//   "Frutos e Fritas do Frota",
+//   "Taxi Aéreo Mamute Pequenino",
+//   "Be a Bar",
+//   "Rock in Reels",
+//   "Gê Estantes",
+//   "Adomicílios",
+//   "Abalada",
+//   "Ricárdio"
+// ];
+
+// empresas.splice(0, 2);
+// empresas.unshift("Cardiopatinha");
+
+// console.log(empresas);
+
+// ================================================================================================
+
+// const pessoas = [
+//   "Dona Bete",
+//   "Kowalski",
+//   "Guilherme Portões",
+//   "Jeferson Bezerra",
+//   "Heitor Tuga",
+//   "Pe. Ernan Buco",
+//   "Frei Dimão",
+//   "Capitão Ganso",
+//   "Junin",
+//   "Gael",
+//   "Sarumano"
+// ];
+
+// pessoas.reverse();
+
+// console.log(pessoas);
+// console.log(pessoas.indexOf("Pe. Ernan Buco"));
+
+// ================================================================================================
+
+// const empresas = [
+//   "A Bem Suada",
+//   "Floricultura Jorge Tadeu",
+//   "Aspirantes Aspiradores",
+//   "Indra",
+//   "jamanjo.com.br",
+//   "MA Canudos",
+//   "Harry Plotter",
+//   "Telles Transportes",
+//   "Rede Eco",
+//   "Hotel Téo"
+// ];
+
+// empresas.push("Pousada Ada");
+// empresas.splice(3, 1);
+
+// console.log(empresas);
+// console.log(empresas.includes("Indra"));
+
+// ================================================================================================
+
+// const empresas = [
+//   "Macrohard",
+//   "Bergamota",
+//   "Amazonia",
+//   "Trajeto Pomar",
+//   "Pé Pequeno",
+//   "Visionery",
+//   "Armando Guerra",
+//   "Samara Poços",
+//   "Paralamas do Seu Celso",
+//   "Plumas de Avalon",
+//   "Vírus da Grife",
+//   "Romero Brique",
+//   "Grades Cida",
+//   "HD Cimentos",
+//   "Meia Meia Meia",
+//   "Food Park Comidas e Clichês",
+//   "Churrascaria do Gaúcho",
+//   "Taberna Periódica"
+// ];
+
+// empresas.splice(2, 1);
+// empresas.unshift("Coxão do Santinho");
+// empresas.push("Tião Megastore");
+// empresas.reverse();
+
+// console.log(empresas.includes("HD Cimentos"));
+// console.log(empresas.indexOf("Macrohard"));
+// console.log(empresas);
